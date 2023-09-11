3c4fa3f650cc6b0211274f9ff51b99d926b289ca
Dan Dev <ifdanthencool@gmail.com>
2023-09-10 22:57:16 -0500

    Updated home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <p className="text-yellow-500">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] dark:text-[#0a192f]">Dan Coria</h1>
         <h2 className="text-4xl sm:text-5xl font-bold text-[#8892b0] my-3 dark:text-[#050f1dc7]">I'm a Front End Developer</h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">I used to believe that being a web developer would involve secluded cubicle work, with headphones on, tirelessly debugging and constructing the next groundbreaking technology that would revolutionize the world. However, as I gained experience over the past 8+ years in the industry, it became clear that I haven't quite had that exact experience. Being a web developer is actually more focused on identifying the optimal solution for the problem at hand. This can involve writing code, and exploring third-party solutions.</p>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">I used to believe that being a web developer would involve secluded cubicle work, with headphones on, tirelessly debugging and constructing the next groundbreaking technology that would revolutionize the world. However, as I gained experience over the past 5+ years in the industry, it became clear that I haven't quite had that exact experience. Being a web developer is actually more focused on identifying the optimal solution for the problem at hand. This can involve writing code, and exploring third-party solutions.</p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-yellow-500 hover:border-yellow-500 dark:bg-yellow-500 dark:border-yellow-500 hover:text-[#0a192f]">
             <Link to="work" smooth={true} duration={500}>
